b248d982507296588de34d9bdde720476548bb8c
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-04-06 10:21:28 -0400

    mvp complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,79 @@
 var config = {
-  apiKey: "AIzaSyDwzah4SNohoCghWl1tC59hGKHRF5ljWBo",
-  authDomain: "train-scheduler-29c99.firebaseapp.com",
-  databaseURL: "https://train-scheduler-29c99.firebaseio.com",
-  projectId: "train-scheduler-29c99",
-  storageBucket: "",
-  messagingSenderId: "514035898658"
+  apiKey: 'AIzaSyDwzah4SNohoCghWl1tC59hGKHRF5ljWBo',
+  authDomain: 'train-scheduler-29c99.firebaseapp.com',
+  databaseURL: 'https://train-scheduler-29c99.firebaseio.com',
+  projectId: 'train-scheduler-29c99',
+  storageBucket: '',
+  messagingSenderId: '514035898658',
 };
-firebase.initializeApp(config);
+firebase.initializeApp (config);
 
-var database = firebase.database();
+var database = firebase.database ();
 
-$("#submit-button").on("click", function () {
+$ ('#submit-button').on ('click', function () {
+  var name = $ ('#name-input').val ().trim ();
+  var destination = $ ('#destination-input').val ().trim ();
+  var firstTrainTime = $ ('#first-train-input').val ().trim ();
+  var frequency = $ ('#frequency-input').val ().trim ();
 
-  var name = $("#name-input").val().trim()
-  var destination = $("#destination-input").val().trim()
-  var firstTrainTime = $("#first-train-input").val().trim()
-  var frequency = $("#frequency-input").val().trim()
-
-  if ("name" != "" && destination != "" && firstTrainTime != "" && frequency != "") {
-
-    database.ref().push({
+  if (
+    'name' != '' &&
+    destination != '' &&
+    firstTrainTime != '' &&
+    frequency != ''
+  ) {
+    database.ref ().push ({
       name: name,
       destination: destination,
       firstTrainTime: firstTrainTime,
-      frequency: frequency
-    })
-
+      frequency: frequency,
+    });
   } else {
-
-    alert("All fields must be filled out!")
-
+    alert ('All fields must be filled out!');
   }
+});
 
-})
+database.ref ().on ('child_added', function (childSnapshot) {
+  var name = childSnapshot.val ().name;
+  var destination = childSnapshot.val ().destination;
+  var firstTrainTime = childSnapshot.val ().firstTrainTime;
+  var frequency = childSnapshot.val ().frequency;
 
-database.ref().on("child_added", function (childSnapshot) {
+  console.log ('current time: ' + moment ().format ('hh:mm'));
 
-  var name = childSnapshot.val().name
-  var destination = childSnapshot.val().destination
-  var firstTrainTime = childSnapshot.val().firstTrainTime
-  var frequency = childSnapshot.val().frequency
+  console.log ('first train time: ' + firstTrainTime);
 
-  console.log("current time: " + moment().format('hh:mm'))
+  var splitFirstTrainTime = firstTrainTime.split(':');
+  var firstTrainTimeHours = splitFirstTrainTime[0];
+  var firstTrainTimeMinutes = splitFirstTrainTime[1];
+  var trainTime = moment ()
+    .hours (firstTrainTimeHours)
+    .minutes (firstTrainTimeMinutes);
 
-  console.log("first train time: " + firstTrainTime)
+  var currentTime = moment();
+  var diffTime = moment().diff(moment(trainTime), "minutes");
+  var remainder = diffTime % frequency;
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+  var MinutesTillTrain = frequency - remainder;
+  console.log("MINUTES TILL TRAIN: " + MinutesTillTrain);
+  var nextArrival = moment().add(MinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+  var convertedNextArrival = moment(nextArrival).format("hh:mm a");
 
-  var convertedFirstTrainTime = (moment(firstTrainTime).format('hh:mm'))
-  var nextTrain = (moment().format('hh:mm').subtract(firstTrainTime))
-
-  console.log("converted first train time: " + convertedFirstTrainTime)
-
-  console.log("next train: " + nextTrain)
-  console.log(moment(convertedFirstTrainTime).toNow())
-
-
-  var newRow = $("<tr>")
-  var newName = $("<td>").text(name)
-  var newDestination = $("<td>").text(destination)
-  var newFrequency = $("<td>").text(frequency)
-  var newNextTrain = $("<td>").text(nextTrain)
-
-  newRow.append(newName)
-  newRow.append(newDestination)
-  newRow.append(newFrequency)
-  newRow.append(newNextTrain)
+  var newRow = $ ('<tr>');
+  var newName = $ ('<td>').text (name);
+  var newDestination = $ ('<td>').text (destination);
+  var newFrequency = $ ('<td>').text (frequency);
+  var newNextArrival = $ ('<td>').text (convertedNextArrival);
+  var newNextTrain = $ ('<td>').text (MinutesTillTrain);
 
 
-  $("tbody").append(newRow)
 
-})
+  newRow.append (newName);
+  newRow.append (newDestination);
+  newRow.append (newFrequency);
+  newRow.append (newNextArrival);
+  newRow.append (newNextTrain);
+
+  $ ('tbody').append (newRow);
+});
